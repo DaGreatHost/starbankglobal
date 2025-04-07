62b142de26ccfa0b7002200e8ca8cd5eaea6cffe
DaGreatHost <heherson230@gmail.com>
2025-04-08 04:59:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,8 +66,7 @@ async def show_main_menu(client, message_or_query, lang):
             for name, data in star_packages.items()]
     text = LANG[lang]["welcome"] + "\n\n" + LANG[lang]["choose_pack"]
 
-text = LANG[lang]["welcome"] + "\\n\\n" + LANG[lang]["choose_pack"]
-
+    text = LANG[lang]["welcome"] + "\n\n" + LANG[lang]["choose_pack"]
 
     if isinstance(message_or_query, Message):
         await message_or_query.reply(text, reply_markup=InlineKeyboardMarkup(buttons), parse_mode="markdown")
